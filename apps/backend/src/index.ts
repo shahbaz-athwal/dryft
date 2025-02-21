c63c6f02a6f96363cee9ab1ba2b708b0b7c45d5b
Shahbaz Singh <shahbazathwal2107@gmail.com>
2025-02-21 18:36:14 -0400

    cache test

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -19,7 +19,7 @@ app.use("/api", routes);
 app.get("/", async (req, res) => {
   const users = await prisma.user.findMany();
   res.json({
-    message: "New Images",
+    message: "New Images 1",
     users,
   });
 });

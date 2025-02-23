3e30db7db8491e1637a0317ffca7698853ff5fa5
Shahbaz Singh <shahbazathwal2107@gmail.com>
2025-02-23 18:39:51 -0400

    auth page built

--- apps/web/src/components/Auth.tsx
+++ apps/web/src/components/Auth.tsx
@@ -1,6 +1,13 @@
 import { SignupForm } from "../components/ui/sign-up";
 import { SignInForm } from "../components/ui/sign-in";
 import { authClient } from "../lib/auth-client";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@repo/ui/components/tabs";
+import { Card, CardContent } from "@repo/ui/components/card";
 
 function Auth() {
   const handleSignupSubmit = async (credentials: {
@@ -55,8 +62,26 @@ function Auth() {
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <SignupForm onSubmit={handleSignupSubmit} />
-      <SignInForm onSubmit={handleSignInSubmit} />
+      <Tabs defaultValue="signup" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="signup">Sign Up</TabsTrigger>
+          <TabsTrigger value="signin">Sign In</TabsTrigger>
+        </TabsList>
+        <TabsContent value="signup">
+          <Card>
+            <CardContent className="p-0">
+              <SignupForm onSubmit={handleSignupSubmit} />
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="signin">
+          <Card>
+            <CardContent className="p-0">
+              <SignInForm onSubmit={handleSignInSubmit} />
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }

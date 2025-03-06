cf019c5303b9d45736bef4a19ee47ee70f03f7f8
Shahbaz Singh <shahbazathwal2107@gmail.com>
2025-03-06 19:05:33 -0400

    fix slow dev mode

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -10,7 +10,11 @@ const PORT = process.env.PORT ?? 3000;
 
 app.use(
   cors({
-    origin: ["https://capstone.shahcodes.in", "http://localhost:5173"],
+    origin: [
+      "https://capstone.shahcodes.in",
+      "http://localhost:5173",
+      "http://localhost:4173",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })

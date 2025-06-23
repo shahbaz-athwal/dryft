5b76144ea890a12940067095fedb21f4deb47385
shahbaz-athwal <shahbazathwal2107@gmail.com>
2025-06-22 21:48:55 -0600

    chore: class based scraper

--- packages/scraper/src/utils/axios.ts
+++ packages/scraper/src/utils/axios.ts
@@ -1,11 +1,7 @@
 import axios, { type AxiosInstance } from "axios";
-import { wrapper } from "axios-cookiejar-support";
 
-// Create a cookie jar and an axios instance that uses it
-export const client: AxiosInstance = wrapper(
-  axios.create({
-    baseURL: "https://collss.acadiau.ca",
-    withCredentials: true,
-    validateStatus: () => true,
-  }),
-);
+export const client: AxiosInstance = axios.create({
+  baseURL: "https://collss.acadiau.ca",
+  withCredentials: true,
+  validateStatus: () => true,
+});

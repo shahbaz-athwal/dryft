3260647d39b907b76c5060dbe32e3067a9eea477
shahbaz-athwal <shahbazathwal2107@gmail.com>
2025-06-23 23:45:57 -0600

    check: test school search success

--- packages/scraper/src/utils/rmp-gql-client.ts
+++ packages/scraper/src/utils/rmp-gql-client.ts
@@ -1,9 +1,7 @@
 import { GraphQLClient } from "graphql-request";
 
-// RateMyProfessors GraphQL endpoint
 const RMP_GRAPHQL_ENDPOINT = "https://www.ratemyprofessors.com/graphql";
 
-// Create GraphQL client with necessary headers
 const client = new GraphQLClient(RMP_GRAPHQL_ENDPOINT, {
   headers: {
     "User-Agent":
@@ -15,4 +13,4 @@ const client = new GraphQLClient(RMP_GRAPHQL_ENDPOINT, {
   },
 });
 
-export { client };
+export { client as gqlClient };

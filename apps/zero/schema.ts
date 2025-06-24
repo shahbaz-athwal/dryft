a7ad3082335d4fa8f1aea3b73d3d519fdd6f7fa1
shahbaz-athwal <shahbazathwal2107@gmail.com>
2025-06-24 13:05:52 -0600

    chore: remove zero

--- apps/zero/schema.ts
+++
@@ -1,8 +0,0 @@
-import { schema as generatedSchema, type Schema } from "@repo/db/schema";
-import { ANYONE_CAN_DO_ANYTHING, definePermissions } from "@rocicorp/zero";
-
-export const schema = generatedSchema;
-
-export const permissions = definePermissions<unknown, Schema>(schema, () => ({
-  user: ANYONE_CAN_DO_ANYTHING,
-}));

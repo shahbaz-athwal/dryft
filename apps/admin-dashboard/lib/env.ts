940152458f6c4e56c13f2afe6df598cf462ab4ea
shahbaz-athwal <shahbazathwal2107@gmail.com>
2025-10-13 11:49:54 -0300

    better envs

---
+++ apps/admin-dashboard/lib/env.ts
@@ -0,0 +1,17 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { vercel } from "@t3-oss/env-nextjs/presets-zod";
+import { z } from "zod";
+
+export const env = createEnv({
+  extends: [vercel()],
+  shared: {
+    NODE_ENV: z.enum(["development", "production"]).default("development"),
+  },
+  client: {
+    NEXT_PUBLIC_API_URL: z.string().min(1),
+  },
+  runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+});
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,12 +1,13 @@
 import { prisma } from "@repo/db/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { authEnv } from "./env";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-
+  secret: authEnv().BETTER_AUTH_SECRET,
   advanced: {
     defaultCookieAttributes: {
       secure: true,

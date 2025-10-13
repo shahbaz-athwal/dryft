998103aab1f72faa83928d326386d77654c5da66
shahbaz-athwal <shahbazathwal2107@gmail.com>
2025-10-13 12:11:17 -0300

    unify env management

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,13 +1,13 @@
 import { prisma } from "@repo/db/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { authEnv } from "./env";
+import { env } from "bun";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-  secret: authEnv().BETTER_AUTH_SECRET,
+  secret: env.BETTER_AUTH_SECRET,
   advanced: {
     defaultCookieAttributes: {
       secure: true,

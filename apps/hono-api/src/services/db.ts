1ead0cd9c18b5f28902b4e5ab3b47e473e90fd00
shahbaz-athwal <shahbazathwal2107@gmail.com>
2025-11-26 17:28:25 -0400

    chore: prisma v7 🚀

--- apps/hono-api/src/services/db.ts
+++ apps/hono-api/src/services/db.ts
@@ -1,19 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../prisma/generated/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const db = new PrismaClient({ adapter });
 
-// eslint-disable-next-line
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-};
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-export { prisma };
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+export { db };

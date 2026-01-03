e1b1574755fec947e27dcc6d78e6742bbb0d4a48
shahbaz-athwal <shahbazathwal2107@gmail.com>
2026-01-03 19:55:57 -0400

    refactor file service

--- apps/hono-api/src/services/file-upload.ts
+++ apps/hono-api/src/services/file-upload.ts
@@ -6,13 +6,17 @@ if (!(env.AWS_ACCESS_KEY_ID && env.AWS_SECRET_ACCESS_KEY)) {
   throw new Error("AWS credentials are not set");
 }
 
+export const s3Client = aws({
+  accessKeyId: env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+  region: "us-east-1",
+});
+
+export const BUCKET_NAME = "acadia-one";
+
 export const uploadRouter: Router = {
-  client: aws({
-    accessKeyId: env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
-    region: "us-east-1",
-  }),
-  bucketName: "acadia-one",
+  client: s3Client,
+  bucketName: BUCKET_NAME,
   routes: {
     images: route({
       fileTypes: ["image/*"],

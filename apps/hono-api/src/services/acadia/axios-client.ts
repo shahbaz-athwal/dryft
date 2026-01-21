5947564653684ff373db91f042102272d27bed7f
shahbaz-athwal <shahbazathwal2107@gmail.com>
2026-01-21 14:53:17 -0400

    fix ssl error

--- apps/hono-api/src/services/acadia/axios-client.ts
+++ apps/hono-api/src/services/acadia/axios-client.ts
@@ -1,9 +1,13 @@
+import https from "node:https";
 import axios from "axios";
 
 const BASE_URL = "https://collss.acadiau.ca";
 
 const client = axios.create({
   baseURL: BASE_URL,
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
   validateStatus: (status) => {
     return status >= 200 && status < 500;
   },
